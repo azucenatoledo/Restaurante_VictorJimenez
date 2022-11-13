de902ccf9f84a5c154fa48fa691515a86fdb4f19
vitacho <victor.a.jimenez@unl.edu.ec>
2022-11-12 23:29:08 -0500

    arreglo de urlpatterns 2

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.models import User
-
+from django.conf.urls.static import static
 from rest_framework import routers, serializers, viewsets
 from rest_framework import routers
 from . import viewss
@@ -34,16 +34,9 @@ urlpatterns = [
     path('profile/',views.ProfilView.as_view(), name='profile'),
     #path('api/', include(router.urls)),
     #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    #archivos estaticos CSS
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += ','
-    #archivos estaticos MEDIA
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     
     
