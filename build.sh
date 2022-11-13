64cccd310e281096b9947bbadcdd4878d00cd97f
vitacho <victor.a.jimenez@unl.edu.ec>
2022-11-12 20:03:08 -0500

    cambiar pillow

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 #poetry install
 pip install --upgrade pip
+pip install --force-reinstall -U setuptools
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
