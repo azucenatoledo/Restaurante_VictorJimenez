d7cf8ae5e3c96a13600079c23ccd2cc8b21ae61c
vitacho <victor.a.jimenez@unl.edu.ec>
2022-11-12 20:35:17 -0500

    subir

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -o errexit
 
 #poetry install
-pip install --upgrade pip
-pip install --force-reinstall -U setuptools
+pip install --upgrade pippip install --force-reinstall -U setuptools
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
