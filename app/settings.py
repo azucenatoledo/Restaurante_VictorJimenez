d41f407d37ade926a22141a99d6986406d9b9edb
vitacho <victor.a.jimenez@unl.edu.ec>
2022-12-19 08:58:54 -0500

    Cambios en settings

--- app/settings.py
+++ app/settings.py
@@ -12,13 +12,13 @@ environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#Prodciion en render
-#SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+# Prodciion en render
+# SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 
-#Ambeinte de preuba
+# Ambeinte de preuba
 SECRET_KEY = env('SECRET_KEY')
-#DEBUG = False
+# DEBUG = False
 DEBUG = 'RENDER' not in os.environ
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,18 +26,17 @@ DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-
-#hostaname de render
+# hostaname de render
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_PORT= 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = 'alfredoji300@gmail.com'
 EMAIL_HOST_PASSWORD = env('GMAIL_PASWORD')
 EMAIL_USE_TLS = True
@@ -62,8 +61,8 @@ INSTALLED_APPS = [
     'cloudinary',
 ]
 
-DEFAULT_FROM_EMAIL=env('DEFAULT_FROM_EMAIL')
-NOTIFY_EMAIL=env('NOTIFY_EMAIL')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+NOTIFY_EMAIL = env('NOTIFY_EMAIL')
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
@@ -87,7 +86,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR),'templates'],
+        'DIRS': [os.path.join(BASE_DIR), 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,11 +101,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-if not DEBUG :
+"""
+if not DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -120,10 +118,7 @@ if not DEBUG :
             'PORT': '5432',
         },
     }
-
-
-
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -144,48 +139,43 @@ AUTH_PASSWORD_VALIDATORS = [
 
 SITE_ID = 1
 AUTHENTICATION_BACKENDS = [
-    
+
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    
-]
-#configuracion de allauth para seguridad en el sistema
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email' #Registro por usuario y contraseña
-ACCOUNT_EMAIL_REQUIRED = True   # Que se tenga que verificar el correo
-ACCOUNT_USERNAME_REQUIRED = True    #Que se pida el nombre de usuario
-ACCOUNT_PRESERVE_USERNAME_CASING =True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS =3
-ACCOUNT_EMAIL_SUBJECT_PREFIX = 'Verificacion'
-ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 3 #limite de intentos de inicio de sesion
 
+]
+# configuracion de allauth para seguridad en el sistema
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'  # Registro por usuario y contraseña
+ACCOUNT_EMAIL_REQUIRED = True  # Que se tenga que verificar el correo
+ACCOUNT_USERNAME_REQUIRED = True  # Que se pida el nombre de usuario
+ACCOUNT_PRESERVE_USERNAME_CASING = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
+ACCOUNT_EMAIL_SUBJECT_PREFIX = 'Verificacion'
+ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 3  # limite de intentos de inicio de sesion
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-CRISPY_TEMPLATE_PACK='bootstrap4'
-LOGIN_REDIRECT_URL='/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = '/'
 
 LANGUAGE_CODE = 'es-ec'
 TIME_ZONE = 'America/Guayaquil'
 USE_I18N = True
 USE_L10N = True
 
+# Cpaypal
+PAYPAL_CLIENT_ID = env('PAYPAL_SANDBOX_CLIENT_ID')
+PAYPAL_SECRET_KET = env('PAYPAL_SANDBOX_SECRET_KEY')
 
-
-#Cpaypal
-PAYPAL_CLIENT_ID=env('PAYPAL_SANDBOX_CLIENT_ID')
-PAYPAL_SECRET_KET=env('PAYPAL_SANDBOX_SECRET_KEY')
-
-
-
-#arichivos estaticos
+# arichivos estaticos
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR,"static")]
-STATIC_ROOT = os.path.join(BASE_DIR,"static_root")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 CLOUDINARY_STORAGE = {
@@ -196,15 +186,13 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-#segurida en el cierre de sesion
-SESSION_COOKIE_AGE = 60 * 60 #Tiempo de vida de la sesión en segundos -> x Minutos x 60segundos.  (60min*60seg)
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True #Si sale del navegador, cerrar sesión
+# segurida en el cierre de sesion
+SESSION_COOKIE_AGE = 60 * 60  # Tiempo de vida de la sesión en segundos -> x Minutos x 60segundos.  (60min*60seg)
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Si sale del navegador, cerrar sesión
 SESSION_SAVE_EVERY_REQUEST = True  # actualizar tiempo de vida en cada request
-
 
 if DEBUG:
     SESSION_COOKIE_SECURE = True
@@ -216,21 +204,19 @@ if DEBUG:
     SECURE_REDIRECT_EXEMPT = []
     SECURE_SSL_REDIRECT = TRUE
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    print(SECURE_SSL_REDIRECT)
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     ####
-    EMAIL_HOST= 'smtp.gmail.com'
-    EMAIL_PORT= 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
     EMAIL_HOST_USER = 'alfredoji300@gmail.com'
     EMAIL_HOST_PASSWORD = env('GMAIL_PASWORD')
     EMAIL_USE_TLS = True
     # Database
     # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-    #configuracion para render en base de datos render
+    # configuracion para render en base de datos render
     DATABASES = {
         'default': dj_database_url.config(
             default='postgresql://postgres:postgres@localhost:5432/postgres',
             conn_max_age=600
         )}
-
