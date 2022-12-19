bf58ae99c86f8339f0b434fd64876c7b42de4cc4
vitacho <victor.a.jimenez@unl.edu.ec>
2022-12-19 09:21:50 -0500

    Cambios en settings False

--- app/settings.py
+++ app/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Ambeinte de preuba
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = True
-#DEBUG = 'RENDER' not in os.environ
+#DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 print(DEBUG)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -195,7 +195,7 @@ SESSION_COOKIE_AGE = 60 * 60  # Tiempo de vida de la sesión en segundos -> x Mi
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True  # Si sale del navegador, cerrar sesión
 SESSION_SAVE_EVERY_REQUEST = True  # actualizar tiempo de vida en cada request
 
-if DEBUG is True:
+if DEBUG is False:
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
